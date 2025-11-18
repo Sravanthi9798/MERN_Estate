c0f3fd5f2b4510844abf2b63145152682821a541
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2025-11-18 11:52:31 +0530

    Install react and tailwind css

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div class="bg-red">
+      <h1 class="text-red-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
