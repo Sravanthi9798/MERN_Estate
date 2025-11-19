c06f30875ca1c41bbffc92388e3e615c263d70a4
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2025-11-19 12:30:15 +0530

    Added server and mongo db connection

---
+++ Server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB).then(()=>{
+  console.log("Connected successfully")
+}).catch((err)=>{
+  console.log(err)
+})
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
