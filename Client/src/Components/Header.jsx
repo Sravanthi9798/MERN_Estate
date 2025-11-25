e8d101aa59d4014e2b95421c65708d23c8e715f5
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2025-11-25 12:00:10 +0530

    Completed signin functionality

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
                     <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                     </Link>
-                    <Link to='/signup'>
+                    <Link to='/signin'>
                     <li className='text-slate-700 hover:underline'>{' '}SignIn</li>
                     </Link>
                 </ul>
--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,43 +1,78 @@
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    // console.log(formData,'formData')
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/auth/signin", formData);
+      if (res.data.success === false) {
+        setLoading(false);
+        setError(res.data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      if (error.response) {
+        setError(error.response.data.message);
+      } else {
+        setError(error.message);
+      }
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <label>Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg border-[#8e6767]"
           id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+          onChange={handleChange}
         />
-      </div>
-      <div>
-        <label>Password:</label>
         <input
           type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg border-[#8e6767]"
           id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
+          onChange={handleChange}
         />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In  "}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to={"/signup"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
