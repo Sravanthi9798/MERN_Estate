3d0ae448650aa5d284a16f0748a7d288c4e4737f
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2025-11-27 11:43:33 +0530

    Added private route to profile page if user is not logged in

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignOut from './pages/SignOut';
-import Profile from './pages/Profile';
-import Header from './Components/Header';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+import Header from "./Components/Header";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./Components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/signout' element={<SignOut />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+    const {currentUser}=useSelector(state=>state.user);
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-2'>
@@ -21,9 +23,9 @@ const Header = () => {
                     </Link>
                     <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-                    </Link>
-                    <Link to='/signin'>
-                    <li className='text-slate-700 hover:underline'>{' '}SignIn</li>
+                    </Link>                  
+                    <Link to='/profile'>
+                    {currentUser ?<img src={currentUser.avatar} alt="profile" className="rounded-full h-7 w-7 object-cover"/>: <li className='text-slate-700 hover:underline'>{' '}SignIn</li>}
                     </Link>
                 </ul>
             </div>
